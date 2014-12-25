c55dd7402d70c716f035eb5634d9f04e34539b90
aacuna3 <aacuna3@asu.edu>
2014-12-25 15:39:44 -0700

    Added Location Update Service

---
+++ app/src/main/java/com/capstone/glassapplication/LocationUpdateService.java
@@ -0,0 +1,16 @@
+package com.capstone.glassapplication;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class LocationUpdateService extends Service {
+    public LocationUpdateService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
